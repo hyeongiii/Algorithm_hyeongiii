f28163cedde5c2def27a723d041280b2ef07fe27
이가형 <101054381+hyeongiii@users.noreply.github.com>
2022-12-21 13:50:42 +0900

    Update 옷가게 할인 받기.py

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,7 +1,21 @@
 def solution(price):
+
+    if price >= 500000:
+        answer = price * 0.8 // 1
+    elif price >= 300000:
+        answer = price * 0.9 // 1
+    elif price >= 100000:
+        answer = price * 0.95 // 1
+    else:
+        answer = price
+    return answer
+
+    '''
+    다른 사람 풀이
     
     discount_dic = {500000 : 0.8, 300000 : 0.9, 100000 : 0.95, 0 : 1}
-    
+
     for discount_price, discount_rate in discount_dic.items():
         if price >= discount_price:
             return int(price * discount_rate)
+    '''        
