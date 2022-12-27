5bd9d6e5a9da2c2e331f2d41a540e25cb34f214d
이가형 <101054381+hyeongiii@users.noreply.github.com>
2022-12-28 01:40:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    if len(arr) == 1:
+        answer = [-1]
+    
+    else:
+        m = min(arr)
+        arr.remove(m)
+        answer = arr
+        
+    return answer
