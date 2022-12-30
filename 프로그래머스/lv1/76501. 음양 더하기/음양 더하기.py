dae46c27ea44aeb1040defa6149de0740b7f439a
이가형 <101054381+hyeongiii@users.noreply.github.com>
2022-12-30 23:31:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,12 +1,3 @@
 def solution(absolutes, signs):
-    answer = 0
-    li = []
-    
-    for i in range(len(signs)):
-        if signs[i] == True:
-            li.append(absolutes[i])
-        else:
-            li.append(-absolutes[i])
-            
-    answer = sum(li)        
+    answer = 123456789
     return answer
