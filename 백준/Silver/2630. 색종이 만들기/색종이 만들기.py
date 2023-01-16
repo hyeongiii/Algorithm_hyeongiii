e3ca06f6170f28ce4fd6386c597c688e977b8aab
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-16 21:59:52 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,32 +1,32 @@
 import sys
+
 input = sys.stdin.readline
 
 # 정수 입력 받기
-N = int(input())
+num = int(input())
 # 색종이 입력 받기
-PAPER = [list(map(int, input().split())) for _ in range(N)]
-answer = [0, 0]
+paper = [list(map(int, input().rstrip().split())) for _ in range(num)]
+
+result = [0, 0]
 
 
-def solution(x, y, N):
-    # 첫번째 종이의 색깔
-    color = PAPER[x][y]
-    for row in range(x, x + N):
-        for col in range(y, y + N):
-            if color != PAPER[row][col]:
-                # 각각 1, 2, 3, 4분면 이동
-                solution(x, y, N // 2)
-                solution(x, y + N // 2, N // 2)
-                solution(x + N // 2, y, N // 2)
-                solution(x + N // 2, y + N // 2, N // 2)
+def solution(x, y, num):
+    color = paper[x][y]  # 첫 번째 색
+    for i in range(x, x + num):
+        for j in range(y, y + num):
+            if color != paper[i][j]:
+                solution(x, y, num // 2)
+                solution(x, y + num // 2, num // 2)
+                solution(x + num // 2, y, num // 2)
+                solution(x + num // 2, y + num // 2, num // 2)
                 return 0
-    # 모든 범위 내애 종이 색깔이 같다면
+    # 모든 범위 내의 종이 색깔이 같을 경우
     if color == 0:
-        answer[0] += 1
+        result[0] += 1
     else:
-        answer[1] += 1
+        result[1] += 1
 
 
-solution(0, 0, N)
-for a in answer:
+solution(0, 0, num)
+for a in result:
     print(a)
