79d9695788bd7eee3d1e80d3db2ca6449704d27c
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-20 14:48:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,7 +1,6 @@
 def solution(n):
     answer = ''
-    if n % 2 == 1:
-        answer = "수박"*(n//2) + "수"
+    if n % 2 == 0:
+        return "수박" * (n // 2)
     else:
-        answer = "수박"*(n//2)
-    return answer
+        return "수박" * (n // 2) + "수"
