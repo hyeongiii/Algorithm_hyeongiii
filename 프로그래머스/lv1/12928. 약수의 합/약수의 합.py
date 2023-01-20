fd8a0dde177fd01eb172847a2d75e0d74258f152
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-20 09:21:41 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,20 +1,8 @@
 def solution(n):
-    li = []
+    answer = []
     
-    '''
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if n % i == 0:
-            li.append(i)
-    '''
-    for i in range(1, int(n **(1/2)) + 1):
-        if n % i == 0:
-            li.append(i)
-            # 몫
-            k = n // i
-            if k != i:
-                li.append(k)
+            answer.append(i)
             
-            
-    answer = sum(li)
-    
-    return answer
+    return sum(answer)
