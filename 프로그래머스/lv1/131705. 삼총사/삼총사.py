09c7eb6101cd8bf622a6f9c362eb8ec5bdb32d21
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-25 15:25:10 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,3 +1,5 @@
+'''
+내가 푼 풀이
 def solution(number):
     answer = 0
     
@@ -7,3 +9,14 @@ def solution(number):
                 if number[i] + number[j] + number[k] == 0:
                     answer += 1
     return answer
+'''
+
+def solution(number):
+    from itertools import combinations    # 조합을 구해주는 함수 사용: combinations
+    cnt = 0
+    
+    for i in combinations(number, 3):
+        if sum(i) == 0:
+            cnt += 1
+            
+    return cnt
