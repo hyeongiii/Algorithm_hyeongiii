ce29f24b2be4761e21557ad26150be5df8d0bedb
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-25 17:19:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.27 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,13 @@
 def solution(participant, completion):
-    answer = ''
-    temp = 0
-    dic = {}
-    for part in participant:
-        dic[hash(part)] = part
-        temp += int(hash(part))
-    for com in completion:
-        temp -= hash(com)
-    answer = dic[temp]
 
-    return answer
+    # 1. 두 list를 정렬한다.
+    participant.sort()
+    completion.sort()
+
+    # completion 와 participant를 비교하여 다를 경우, 그 때의 i 값이 완주하지 못한 선수이다.
+    for i, j in zip(participant, completion):
+        if i != j:
+            return i
+
+    # 만약 completion을 다 순회하도록 i와 j가 같다면, 완주하지 못한 선수는 participant의 제일 마지막 인덱스에 있는 값이다.
+    return participant[-1]
