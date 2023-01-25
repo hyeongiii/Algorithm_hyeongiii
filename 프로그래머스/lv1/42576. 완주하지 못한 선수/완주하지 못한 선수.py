6d28a4d4fb4490cd673edb4034c4af0b8c31ebf6
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-25 18:00:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.91 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,24 @@
+# hash 활용
 def solution(participant, completion):
+    answer = ''
+    sumHash = 0
+    dic = {}
 
-    # 1. 두 list를 정렬한다.
-    participant.sort()
-    completion.sort()
+    for part in participant:
+        dic[hash(part)] = part
+        sumHash += int(hash(part))
 
-    # completion 와 participant를 비교하여 다를 경우, 그 때의 i 값이 완주하지 못한 선수이다.
-    for i, j in zip(participant, completion):
-        if i != j:
-            return i
+    for com in completion:
+        sumHash -= hash(com)
+        
+    answer = dic[sumHash]
+    
+    return answer
 
-    # 만약 completion을 다 순회하도록 i와 j가 같다면, 완주하지 못한 선수는 participant의 제일 마지막 인덱스에 있는 값이다.
-    return participant[-1]
+'''
+HashMap : key-value 짝을 이뤄 관리하는 클래스로, 이 문제에서 key는 hash한 값이고 value는 각 선수의 이름이다.
+HashMap에 각 이름의 hash값을 구하고 'hashing'한 값을 모두 더한 것이 sumHash가 된다.
+
+completion을 순회하며 각 이름들의 hash 값을 빼주면, 결국 완주하지 않은 선수의 hash 값만 남게 된다.
+그 때, HashMap에서 해당 해시값을 key로 가진 이름을 반환하면 답이다.
+'''
