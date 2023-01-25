c296cfabd18526a3eaaef062eb3c6c78ed160f65
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-25 15:57:47 +0900

    [level 1] Title: 폰켓몬, Time: 1.50 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,12 @@
+def solution(nums):
+    num = len(nums) // 2    # 가져갈 폰켓몬의 수
+    box = []    # 서로 다른 종류의 폰켓몬이 들어갈 수 있는 박스
+    
+    for i in nums:
+        if not i in box:    # 만약 박스 내에 해당 종류의 폰켓몬이 없다면, 박스내로 주입
+            box.append(i)
+    
+    if len(box) < num:    # 박스 내 폰켓몬 수 > 가져가야할 수, 폰켓몬 종류의 최댓값 = 박스 내의 폰켓몬 수
+        return len(box)
+    else:    # 박스 내 폰켓몬 수 <= 가져가야할 수, 폰켓몬 종류의 최댓값 = 가져갈 폰켓몬의 수
+        return num
