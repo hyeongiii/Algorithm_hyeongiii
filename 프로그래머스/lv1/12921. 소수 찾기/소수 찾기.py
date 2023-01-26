9d9d3bf895fc1a8277bbb8209b9ce1770275a5dd
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-26 09:24:01 +0900

    Update 소수 찾기.py

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -4,7 +4,7 @@ def solution(n):
     # 에라토스테네스의 체 알고리즘 (1부터 n까지의 자연수 중 소수 몽땅 구하기)
     for i in range(2, n + 1):
         if i in num:
-            num -= set(range(2*i, n + 1, i))
+            num -= set(range(i * 2, n + 1, i))    # i는 소수, i * 2 부터 n+ 1 범위 내의 i 배수를 제거
     
 
     return len(num)
