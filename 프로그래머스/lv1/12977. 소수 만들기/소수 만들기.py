7c579f4505e1c42d63308b414bab0d70f3f167b3
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-27 13:48:38 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,20 @@
+import itertools
+
+def solution(nums):
+    cnt = 0
+    
+    # itertools.combinations(데이터, n) : 조합 만들기
+    for i in itertools.combinations(nums, 3):
+        s = sum(i)        
+        tmp = 0
+        
+        # 세 수의 합이 소수인지 판별
+        for i in range(2, s//2 + 1):
+            if s % i == 0:
+                tmp += 1
+                break
+        
+        if tmp == 0:
+            cnt += 1
+
+    return cnt
