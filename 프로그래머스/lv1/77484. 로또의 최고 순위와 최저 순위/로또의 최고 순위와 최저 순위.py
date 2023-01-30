a07fd9faae5d5806a9da216278e8a8c3ac42fa54
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-30 16:55:23 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -6,18 +6,8 @@ def solution(lottos, win_nums):
     maxi = len(win) + num
     mini = len(win)
     
+    rank = [6, 6, 5, 4, 3, 2, 1]
     for i in (maxi, mini):
-        if i == 6:
-            answer.append(1)
-        elif i == 5:
-            answer.append(2)
-        elif i == 4:
-            answer.append(3)
-        elif i == 3:
-            answer.append(4)
-        elif i == 2:
-            answer.append(5)
-        else:
-            answer.append(6)
+        answer.append(rank[i])
     
     return answer
