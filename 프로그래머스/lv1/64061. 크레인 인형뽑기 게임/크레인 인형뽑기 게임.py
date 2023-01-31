e38a08ebc19e5177c55d4cd5e1a4248dd396fbd5
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-01-31 09:15:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.65 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,17 +1,19 @@
 def solution(board, moves):
-    basket = []
     answer = 0
+    
+    moves = [i - 1 for i in moves]
+    bucket = []
     
     for i in moves:
         for j in range(len(board)):
-            if board[j][i - 1] != 0:
-                basket.append(board[j][i - 1])    # 0이 아닌 경우에만 인형 뽑기
-                board[j][i - 1] = 0    # 뽑은 후, 해당 자리는 0으로 변경
+            if board[j][i] != 0:
+                if len(bucket) != 0 and board[j][i] == bucket[-1]:
+                    answer += 2
+                    bucket.pop()
+                else:
+                    bucket.append(board[j][i])
+                    
+                board[j][i] = 0
                 break
-                
-        if len(basket) > 1:
-            if basket[-1] == basket[-2]:
-                basket.pop()
-                basket.pop()
-                answer += 2
+    
     return answer
