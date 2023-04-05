2073d90a040f81b203c05c801b67136916f2cbfd
이가형 <101054381+hyeongiii@users.noreply.github.com>
2023-04-05 22:54:18 +0900

    [Unrated] Title: 기찍 N, Time: 928 ms, Memory: 30700 KB -BaekjoonHub

---
+++ 백준/Unrated/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static int N;
+    
+    static void input() {
+        N = scan.nextInt();
+    }
+    
+    static void solution(int N) {
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    
+    public static void main(String[] args) {
+        input();
+        solution(N);
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
